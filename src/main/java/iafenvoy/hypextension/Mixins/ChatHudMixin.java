e06b1ef0d9b4a30876ecb2dabb5406b16c2d2a1b
IAFEnvoy <tanksheng545@126.com>
2022-04-22 23:56:47 +0800

    Add Chat filter

--- src/main/java/iafenvoy/hypextension/Mixins/ChatHudMixin.java
+++ src/main/java/iafenvoy/hypextension/Mixins/ChatHudMixin.java
@@ -2,12 +2,15 @@ package iafenvoy.hypextension.Mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import iafenvoy.hypextension.Config.Configs;
 import iafenvoy.hypextension.Functions.AutoFriend;
 import iafenvoy.hypextension.Functions.AutoGG;
+import iafenvoy.hypextension.Functions.ChatFilter;
 import iafenvoy.hypextension.Utils.MiscUtils;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.ChatHud;
@@ -18,16 +21,24 @@ import net.minecraft.text.Text;
 public abstract class ChatHudMixin extends DrawableHelper {
   @ModifyVariable(method = "addMessage(Lnet/minecraft/text/Text;I)V", at = @At("HEAD"), argsOnly = true)
   private Text addMessageHandler(Text componentIn) {
-    if (Configs.autoGG.getBooleanValue())
-      AutoGG.checkMessage(componentIn.getString());
-    if (Configs.autoFriend.getBooleanValue())
-      AutoFriend.checkMessage(componentIn.getString());
     if (Configs.chatTimeStamp.getBooleanValue()) {
       LiteralText newComponent = new LiteralText(MiscUtils.getChatTimestamp() + " ");
       newComponent.append(componentIn);
       return newComponent;
     }
     return componentIn;
+  }
+
+  @Inject(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At("HEAD"), cancellable = true)
+  private void addMessage(Text message, int messageId, int timestamp, boolean refresh, CallbackInfo ret) {
+    if (Configs.chatFilter.getBooleanValue() && !ChatFilter.checkMessage(message.getString())) {
+      System.out.println("Chat Blocked !");
+      ret.cancel();
+    }
+    if (Configs.autoGG.getBooleanValue())
+      AutoGG.checkMessage(message.getString());
+    if (Configs.autoFriend.getBooleanValue())
+      AutoFriend.checkMessage(message.getString());
   }
 
   @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;fill(Lnet/minecraft/client/util/math/MatrixStack;IIIII)V", ordinal = 0))
