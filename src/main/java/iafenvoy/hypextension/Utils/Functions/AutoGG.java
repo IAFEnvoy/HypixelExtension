0558138bec0cefe472b564d4aef9a9e7d47bfe10
IAFEnvoy <tanksheng545@126.com>
2022-05-18 13:40:37 +0800

    develop auto gg

--- src/main/java/iafenvoy/hypextension/Utils/Functions/AutoGG.java
+++ src/main/java/iafenvoy/hypextension/Utils/Functions/AutoGG.java
@@ -8,26 +8,39 @@ public class AutoGG {
   private static String previousMessage = "";
   private static long prev = 0L;
   private static long coolDown = 0L;
+  private static boolean shouldSend = false;
+  private static int count = 0;
 
   public static void checkMessage(String message) {
     if (message.contains("The game starts in"))
       prev = System.currentTimeMillis();
     if (previousMessage.contains(lineString)) {
-      System.out.println("[Hypixel Extension] Get Line String !");
-      long now = System.currentTimeMillis();
-      if (prev != 0L) {
-        if (now - prev <= 1500L) {
+      if (previousMessage.split(lineString).length > 2 || shouldSend) {
+        count = 0;
+        shouldSend = false;
+      } else {
+        shouldSend = true;
+        long now = System.currentTimeMillis();
+        if (prev != 0L) {
+          if (now - prev <= 1500L) {
+            prev = 0L;
+            previousMessage = message;
+            return;
+          }
           prev = 0L;
-          previousMessage = message;
-          return;
         }
-        prev = 0L;
+        if (MiniGame.containGameName(message)) {
+          if (now - coolDown >= 1500L)
+            ClientUtil.sendMessage("gg", true);
+          coolDown = now;
+        }
       }
-      if (MiniGame.containGameName(message)) {
-        if (now - coolDown >= 1500L)
-          ClientUtil.sendMessage("gg", true);
-        coolDown = now;
-      }
+    }
+    if (shouldSend)
+      count++;
+    if (count > 15) {
+      count = 0;
+      shouldSend = false;
     }
     previousMessage = message;
   }
